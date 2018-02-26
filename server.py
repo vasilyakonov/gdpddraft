c4b9b9ff1843f5915ad8514787bc4fc9c49e29c7
Glitch (because-iam-worthit) <none>
2018-02-26 21:06:18 +0000

    🌁🌂 Checkpoint server.py:687657/230

--- server.py
+++ server.py
@@ -32,36 +32,35 @@ logfile_name = bot_username + ".log"
 
 
 def create_tweet():
-    """Create the text of the tweet you want to send."""
-    # Replace this with your code!
-    text = ""
-    return text
+  """Create the text of the tweet you want to send."""
+  # Replace this with your code!
+  text = "Monkey"
+  return text
 
 
 def tweet(text):
-    """Send out the text as a tweet."""
-    # Twitter authentication
-    auth = tweepy.OAuthHandler(os.environ['C_KEY'], os.environ['C_SECRET'])
-    auth.set_access_token(os.environ['A_TOKEN'], os.environ['A_TOKEN_SECRET'])
-    api = tweepy.API(auth)
+  """Send out the text as a tweet."""
+  # Twitter authentication
+  auth = tweepy.OAuthHandler(os.environ['C_KEY'], os.environ['C_SECRET'])
+  auth.set_access_token(os.environ['A_TOKEN'], os.environ['A_TOKEN_SECRET'])
+  api = tweepy.API(auth)
 
     # Send the tweet and log success or failure
-    try:
-        api.update_status(text)
-    except tweepy.error.TweepError as e:
-        log(e.message)
-    else:
-        log("Tweeted: " + text)
+  # try:
+  api.update_status(text)
+  # except tweepy.error.TweepError as e:
+    # log(e.message)
+  # else:
+    # log("Tweeted: " + text)
 
-
-def log(message):
-    """Log message to logfile."""
-    path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    with open(os.path.join(path, logfile_name), 'a+') as f:
-        t = strftime("%d %b %Y %H:%M:%S", gmtime())
-        f.write("\n" + t + " " + message)
+# def log(message):
+#   """Log message to logfile."""
+#   path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+#   with open(os.path.join(path, logfile_name), 'a+') as f:
+#     t = strftime("%d %b %Y %H:%M:%S", gmtime())
+#     f.write("\n" + t + " " + message)
 
 
 if __name__ == "__main__":
-    tweet_text = create_tweet()
-    tweet(tweet_text)
+  tweet_text = create_tweet()
+  tweet(tweet_text)
