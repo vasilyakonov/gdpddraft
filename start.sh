b7cc394ebb01f82a0205a699d6f2d9e17a2b2af8
Glitch (because-iam-worthit) <none>
2018-02-27 04:30:23 +0000

    😨🏠 Checkpoint start.sh:687657/12 .gitignore:687657/8

--- start.sh
+++ start.sh
@@ -1,24 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-# Exit early on errors
-set -eu
-
-# Python buffers stdout. Without this, you won't see what you "print" in the Activity Logs
-export PYTHONUNBUFFERED=true
-
-# Install Python 3 virtual env
-VIRTUALENV=.data/venv
-
-if ! command -v virtualenv; then
-  pip install virtualenv
-fi
-
-if [ ! -d $VIRTUALENV ]; then
-  virtualenv -p python3 $VIRTUALENV
-fi
-
-# Install the requirements
-$VIRTUALENV/bin/pip install -r requirements.txt
-
-# Run a glorious Python 3 server
-$VIRTUALENV/bin/python3 server.py && sleep 15m
+python server.py && sleep 15m
