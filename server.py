7d3c362c3c3eb578e532ec972710cfcc33b100e1
Glitch (sedate-ocelot) <none>
2018-02-27 12:52:04 +0000

    💷⛱ Checkpoint server.py:706703/64 start.sh:706703/3

--- server.py
+++ server.py
@@ -3,10 +3,19 @@ import os
 import tweepy
 
 
-def create tweet():
-   filename = 'e813cbb64ced5d7d8cc24a0789720729.jpg'
-  
-   the_image = request.get(filename)
+def tweet_image(url, message):
+    api = twitter_api()
+    filename = 'temp.jpg'
+    request = requests.get(url, stream=True)
+    if request.status_code == 200:
+        with open(filename, 'wb') as image:
+            for chunk in request:
+                image.write(chunk)
+
+        api.update_with_media(filename)
+        os.remove(filename)
+    else:
+        print("Unable to download image")
 
 
 #def tweet_image(url, message):
@@ -46,7 +55,12 @@ def tweet(text):
 #     t = strftime("%d %b %Y %H:%M:%S", gmtime())
 #     f.write("\n" + t + " " + message)
 
+
+
+#url = "https://cdn.glitch.com/c8f6cb80-020b-4743-b6ab-6e2bd79b5782%2Fe813cbb64ced5d7d8cc24a0789720729.jpg?1519725876589"
+#message = "Nice one"
+#tweet_image(url, message)
 if __name__ == "__main__":
-  tweet_text = create_tweet()
-  tweet(tweet_text)
+  tweet_image = create_tweet()
+  tweet(tweet_image)
   
