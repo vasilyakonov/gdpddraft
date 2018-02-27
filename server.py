7f943c41cb13d22f515f04217c4d7a8026ec2961
Glitch (sedate-ocelot) <none>
2018-02-27 13:24:10 +0000

    💿😱 Checkpoint start.sh:706703/9 server.py:706703/89

--- server.py
+++ server.py
@@ -2,15 +2,21 @@ import requests
 import os
 import tweepy
 
+def tweet(text):
+  """Send out the text as a tweet."""
+  # Twitter authentication
+  auth = tweepy.OAuthHandler(os.environ['C_KEY'], os.environ['C_SECRET'])
+  auth.set_access_token(os.environ['A_TOKEN'], os.environ['A_TOKEN_SECRET'])
+  api = tweepy.API(auth)
 
-def tweet_image(url, message):
-    url = "https://cdn.glitch.com/c8f6cb80-020b-4743-b6ab-6e2bd79b5782%2Fe813cbb64ced5d7d8cc24a0789720729.jpg?1519725876589"
-    api = tweepy.API()
+  # Send the tweet and log success or failure
+  # try:
+  api.update_with_media(filename)
+  
+def tweet_image():
     filename = 'e813cbb64ced5d7d8cc24a0789720729.jpg'
-    request = requests.get(url, stream=True)
-    if request.status_code == 200:
-        with open(filename, 'wb') as image:
-            for chunk in request:
+    with open(filename, 'wb') as image:
+      for chunk in request:
                 image.write(chunk)
 
         api.update_with_media(filename)
@@ -18,7 +24,7 @@ def tweet_image(url, message):
     else:
         print("Unable to download image")
         
-    return the_tweet
+  
 
 
 #def tweet_image(url, message):
@@ -35,17 +41,8 @@ def tweet_image(url, message):
     #else:
        # print("Unable to download image")
       
-def tweet(text):
-  """Send out the text as a tweet."""
-  # Twitter authentication
-  auth = tweepy.OAuthHandler(os.environ['C_KEY'], os.environ['C_SECRET'])
-  auth.set_access_token(os.environ['A_TOKEN'], os.environ['A_TOKEN_SECRET'])
-  api = tweepy.API(auth)
 
-  # Send the tweet and log success or failure
-  # try:
-  api.update_with_media(filename)
-  os.remove(filename)
+    
   # except tweepy.error.TweepError as e:
     # log(e.message)
   # else:
@@ -65,6 +62,10 @@ def tweet(text):
 #tweet_image(url, message)
 url = "https://cdn.glitch.com/c8f6cb80-020b-4743-b6ab-6e2bd79b5782%2Fe813cbb64ced5d7d8cc24a0789720729.jpg?1519725876589"
 message = "1"
+
+filename = 'e813cbb64ced5d7d8cc24a0789720729.jpg'
+text = "Hello!"
+
 if __name__ == "__main__":
   tweet_image = tweet_image(url,message)
   tweet(tweet_image)
