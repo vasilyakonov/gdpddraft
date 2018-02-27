8883262977185ed1d7a56261fe76f2d28b138f88
Glitch (sedate-ocelot) <none>
2018-02-27 10:16:30 +0000

    🏘🏟 Checkpoint server.py:706703/56 .env:706703/2

--- server.py
+++ server.py
@@ -1,73 +1,30 @@
-# -*- coding: utf-8 -*-
-
+import requests
 import os
 import tweepy
-import requests
-import json
-import random
-from time import gmtime, strftime
 
 
-# ====== Individual bot configuration ==========================
-bot_username = '@XmrpibbX'
-logfile_name = bot_username + ".log"
-# ==============================================================
-
-def create_tweet():
-  """Create the text of the tweet you want to send."""
-  # Replace this with your code!
-  
-  bird_response = requests.get('https://botwiki.org/api/corpora/data/animals/birds_north_america.json')
-  the_birds = bird_response.json()
-  bird_list = []
-  
-  for f in the_birds['birds']:
-    for bird in f['members']:
-      bird_list.append(bird)
-
-  n = random.randint(0,len(bird_list))
-  the_bird = bird_list[n]
-
-  plant_response = requests.get('https://botwiki.org/api/corpora/data/plants/plants.json')
-  the_plants = plant_response.json()
-  plant_list = []
-
-  for p in the_plants['instruments']:
-    plant_list.append(p['name'])
-
-  m = random.randint(0,len(plant_list))
-  the_plant = plant_list[m]
-
-  the_tweet = "A %s samiyam commence %s" % (the_bird,the_plant)
-  
-  img = "http://animalia-life.com/data_images/bird/bird1.jpg"
-  api.update_with_media(img, status="Nice one")
-  
-  return the_tweet
+def twitter_api():
+    auth = tweepy.OAuthHandler(os.environ['C_KEY'], os.environ['C_SECRET'])
+    auth.set_access_token(os.environ['A_TOKEN'], os.environ['A_TOKEN_SECRET'])
+    api = API(auth)
+    return api
 
 
-def tweet(text):
-  """Send out the text as a tweet."""
-  # Twitter authentication
-  auth = tweepy.OAuthHandler(os.environ['C_KEY'], os.environ['C_SECRET'])
-  auth.set_access_token(os.environ['A_TOKEN'], os.environ['A_TOKEN_SECRET'])
-  api = tweepy.API(auth)
+def tweet_image(url, message):
+    api = tweepy.API(auth)
+    filename = 'e813cbb64ced5d7d8cc24a0789720729.jpg'
+    request = assets.get(url, stream=True)
+    if request.status_code == 200:
+        with open(filename, 'wb') as image:
+            for chunk in request:
+                image.write(chunk)
 
-  # Send the tweet and log success or failure
-  # try:
-  api.update_status(text)
-  # except tweepy.error.TweepError as e:
-    # log(e.message)
-  # else:
-    # log("Tweeted: " + text)
+        api.update_with_media(filename, status=message)
+        os.remove(filename)
+    else:
+        print("Unable to download image")
 
-# def log(message):
-#   """Log message to logfile."""
-#   path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-#   with open(os.path.join(path, logfile_name), 'a+') as f:
-#     t = strftime("%d %b %Y %H:%M:%S", gmtime())
-#     f.write("\n" + t + " " + message)
 
-if __name__ == "__main__":
-  tweet_text = create_tweet()
-  tweet(tweet_text)
+url = "https://cdn.glitch.com/c8f6cb80-020b-4743-b6ab-6e2bd79b5782%2Fe813cbb64ced5d7d8cc24a0789720729.jpg?1519725876589"
+message = "Nice one"
+tweet_image(url, message)
