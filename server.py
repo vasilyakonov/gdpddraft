1b97ea9bffe39c11a6c7ec638d3bf966fea28894
Glitch (sedate-ocelot) <none>
2018-02-27 14:17:25 +0000

    🏘⛺️ Checkpoint start.sh:706703/14 server.py:706703/201

--- server.py
+++ server.py
@@ -13,14 +13,15 @@ def tweet(file,text):
   api = tweepy.API(auth)
 
   api.update_with_media(file,text)
+  # you should read the img directory and delete file after posting
   
 def tweet_image():
     url = 'https://kotofoto.ru/UserFiles/Image/category/3100/138527_vinilovaya_plastinka_joy_division_unknown_pleasuresm.jpg'
     response = requests.get(url, stream=True)
+    # you shold be able to specify a path, check glitch support for writing to ASSETS or to .tmp folder
     with open('img.jpg', 'wb') as out_file:
       shutil.copyfileobj(response.raw, out_file)
       del response
-    # filename = urllib.urlopen('https://kotofoto.ru/UserFiles/Image/category/3100/138527_vinilovaya_plastinka_joy_division_unknown_pleasuresm.jpg')
     filename = 'img.jpg'
     text = "Hello!"
     
