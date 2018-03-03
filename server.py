af3093063df6dce1a0d7f7f529c0d01b99b7616f
Glitch (evening-hawk) <none>
2018-03-03 22:09:35 +0000

    🚖👦 Checkpoint server.py:706703/57 start.sh:706703/12

--- server.py
+++ server.py
@@ -15,5 +15,5 @@ import dropbox
 
 dbx = dropbox.Dropbox(os.environ['A_TOKEN2'])
 dbx.users_get_current_account()
-dbx.files_download("cool-file.json, rev=None)
-
+dbx.files_download('/Cavs vs Warriors/Game 5/cool-file.json')
+#print(dbx.files_get_metadata('/KIng of Carrot Flowers Pt2/cool-file.json').server_modified)
