11bf9621b532d0904f9324e6e37c92f462b5e7d4
Glitch (sedate-ocelot) <none>
2018-03-03 20:07:06 +0000

    🕌🐮 Checkpoint server.py:706703/63 start.sh:706703/10

--- server.py
+++ server.py
@@ -26,8 +26,13 @@ def tweet(file,text):
   # you should read the img directory and delete file after posting
   
 def tweet_image():
+    #url = 'https://github.com/vasilyakonov/sedatedocelot/blob/glitch/my-folder/cool-file.json'
+    #response = requests.get(url, stream=True)
+     #you shold be able to specify a path, check glitch support for writing to ASSETS or to .tmp folder
+    #with open('cool.json', 'wb') as out_file:
+      #shutil.copyfileobj(response.raw, out_file)
   
-    images_response = requests.get('https://botwiki.org/corpora-api/')
+    images_response = requests.get('/app/cool.json')
     the_images = images_response.json()
     images_list = []
   
