a3f3937f84cc8b000ffc97022b6c96295bbf1f32
Glitch (sedate-ocelot) <none>
2018-03-03 16:28:31 +0000

    👼👧 Checkpoint server.py:706703/122 start.sh:706703/16

--- server.py
+++ server.py
@@ -31,7 +31,7 @@ def tweet_image():
 
       draw = ImageDraw.Draw(im)
       draw.line((0, 0) + im.size, fill=128)
-      draw.line((0, im.size[1], im.size[0], 0), fill=128)
+      draw.line((0, im.size[1], im.size[0], 0), fill=(1, 1, 1))
       del draw
 
 # write to stdout
