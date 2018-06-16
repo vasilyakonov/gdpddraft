fea6643123712abbad543f0e81c0573d238faa74
Glitch (bright-art) <none>
2018-06-16 15:38:26 +0000

    👏🐯 Checkpoint ./home.html:706703/114 ./start.sh:706703/6 ./server.py:706703/410 ./templates/about.html:706703/142 ./templates/layout.html:706703/477

--- server.py
+++ server.py
@@ -6,12 +6,14 @@ import random
 import flask
 from flask import Flask
 
-
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Hey there!"
+    return render_template('home.html')
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 if __name__ == '__main__':
     app.run(debug=True)
 
