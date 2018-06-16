96d26703f68863aa6f0bacaf7ff3471a4520e275
Glitch (bright-art) <none>
2018-06-16 15:27:40 +0000

    🏆🚠 Checkpoint ./server.py:706703/2184 ./requirements.txt:706703/6 ./start.sh:706703/2

--- server.py
+++ server.py
@@ -1,73 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import os
-import tweepy
 import requests
-import urllib
-import json
 import random
-from time import gmtime, strftime
+import flask
+from flask import Flask
 
 
-# ====== Individual bot configuration ==========================
-bot_username = '@XmrpibbX'
-logfile_name = bot_username + ".log"
-# ==============================================================
-
-def create_tweet():
-  """Create the text of the tweet you want to send."""
-  # Replace this with your code!
-  
-  municipalities_response = requests.get('https://botwiki.org/api/corpora/data/geography/canadian_municipalities.json')
-  the_municipalities = municipalities_response.json()
-  municipalities_list = []
-  
-  for f in the_municipalities["municipalities"]:
-    for k, v in f.iteritems():
-      municipalities_list.append(v)
-
-  n = random.randint(0,len(municipalities_list))
-  the_municipality = municipalities_list[n]
-
-  plant_response = requests.get('https://botwiki.org/api/corpora/data/plants/plants.json')
-  the_plants = plant_response.json()
-  plant_list = []
-
-  for p in the_plants['instruments']:
-    plant_list.append(p['name'])
-
-  m = random.randint(0,len(plant_list))
-  the_plant = plant_list[m]
-
-  the_tweet = "A %s samiyam %s commencement %s" % (the_municipality,the_plant)
-  
-  
-  
-  return the_tweet
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Hey there!"
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
-def tweet(text):
-  """Send out the text as a tweet."""
-  # Twitter authentication
-  auth = tweepy.OAuthHandler(os.environ['C_KEY'], os.environ['C_SECRET'])
-  auth.set_access_token(os.environ['A_TOKEN'], os.environ['A_TOKEN_SECRET'])
-  api = tweepy.API(auth)
-
-  # Send the tweet and log success or failure
-  # try:
-  api.update_status(text)
-  # except tweepy.error.TweepError as e:
-    # log(e.message)
-  # else:
-    # log("Tweeted: " + text)
-
-# def log(message):
-#   """Log message to logfile."""
-#   path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-#   with open(os.path.join(path, logfile_name), 'a+') as f:
-#     t = strftime("%d %b %Y %H:%M:%S", gmtime())
-#     f.write("\n" + t + " " + message)
-
-if __name__ == "__main__":
-  tweet_text = create_tweet()
-  tweet(tweet_text)
