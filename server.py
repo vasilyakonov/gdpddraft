dc5ee80cfd80cf1afd3112e43fd4dc3a14fe889e
Glitch (bright-art) <none>
2018-06-20 16:18:01 +0000

    🐀🐘 Checkpoint ./templates/home.html:706703/1718 ./server.py:706703/572 ./start.sh:706703/5 ./.glitch-assets:706703/398

--- server.py
+++ server.py
@@ -4,7 +4,13 @@ import os
 import requests
 import random
 import flask
+import shutil
 from flask import Flask
+url = 'https://cdn.glitch.com/731b2b86-97c7-4581-83c3-cb37cd854060%2Fohueligrid.pdf?1529511144910'
+response = requests.get(url, stream=True)
+ #you shold be able to specify a path, check glitch support for writing to ASSETS or to .tmp folder
+with open('ohueligrid.pdf', 'wb') as out_file:
+  shutil.copyfileobj(response.raw, out_file)
 
 from flask import Flask, render_template
 app = Flask(__name__)
@@ -16,5 +22,6 @@ def about():
     return render_template('about.html')
 if __name__ == '__main__':
     app.run(debug=False)
+    
 
 
