4ac51b00be137ee8bf4576b78d4dbb680924880f
Glitch (bright-art) <none>
2019-02-18 08:27:13 +0000

    🐦🍟 Checkpoint ./templates/index.html:706703/27659 ./server.py:706703/90 ./templates/home.html:706703/41998 ./templates/layout.html:706703/174

--- server.py
+++ server.py
@@ -15,8 +15,7 @@ def home():
     return render_template('home.html')
 @app.route('/about/')
 def about():
-    return render_template('about.html')
-@app.route('/index/')
+  
 def index():
      return render_template('index.html')
   
