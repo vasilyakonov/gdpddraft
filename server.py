22f6e9b24e29cbe7212f06d3e309184ab82aa85f
Glitch (bright-art) <none>
2019-02-18 08:05:14 +0000

    🌠🚤 Checkpoint ./static/css/main.css:706703/1678 ./templates/index.html:706703/11903 ./server.py:706703/126 ./templates/layout.html:706703/967

--- server.py
+++ server.py
@@ -16,6 +16,10 @@ def home():
 @app.route('/about/')
 def about():
     return render_template('about.html')
+@app.route('/index/')
+def index():
+     return render_template('index.html')
+  
 
 if __name__ == '__main__':
     app.run(debug=False)
