16c4f41bf33161cedd323bef3267088263a9a122
Glitch (bright-art) <none>
2019-02-18 08:38:11 +0000

    💷🏄 Checkpoint ./server.py:706703/812 ./requirements.txt:706703/7 ./templates/index.html:706703/1709

--- server.py
+++ server.py
@@ -6,16 +6,40 @@ import random
 import flask
 import shutil
 import bs4
+import jinja2
+
+
+
+from bs4 import BeautifulSoup 
+
+# load the file
+with open("./templates/index.html") as inf:
+    txt = inf.read()
+    soup = bs4.BeautifulSoup(txt, features="html.parser")
+    print(soup.prettify())
+
+# create new link
+new_link = soup.new_tag("link", rel="icon", type="image/png", href="img/tor.png")
+# insert it into the document
+soup.div.append(new_link)
+
+# save the file again
+with open("./templates/index.html", "w") as outf:
+    outf.write(str(soup))
+    print(soup.prettify())
+
+
+
+
+
+
+
 from flask import Flask
 
 from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route('/')
-def home():
-    return render_template('home.html')
-@app.route('/about/')
-def about():
-  
 def index():
      return render_template('index.html')
   
