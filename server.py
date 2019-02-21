b2c27cb464137c62f234d69dbcfd2bf885292cb3
Glitch (bright-art) <none>
2019-02-21 18:25:12 +0000

    🎺🌁 Checkpoint ./static/rewrite-main.css:706703/2 ./server.py:706703/8 ./templates/index.html:706703/8

--- server.py
+++ server.py
@@ -97,9 +97,6 @@ soup.main.append(object8)
 entries_list.remove(object8)
 
 
-
-
-
 # save the file again
 with open("./templates/index.html", "w") as outf:
     outf.write(str(soup.prettify(formatter=None)))
@@ -121,6 +118,7 @@ def calendar():
 
 if __name__ == '__main__':
     app.run(debug=False)
+    
     
     
 subprocess.call("refresh") 
