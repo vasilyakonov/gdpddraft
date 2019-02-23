bc34bfc08439683902ef42f6263cea2c9f41c70f
Glitch (gdpd-draft) <none>
2019-02-23 17:09:32 +0000

    🚘✈️ Checkpoint ./static/main.css:706703/76 ./server.py:706703/12 ./templates/index.html:706703/22

--- server.py
+++ server.py
@@ -16,7 +16,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def index():
      return render_template('index.html')
@@ -28,8 +27,10 @@ def calendar():
 if __name__ == '__main__':
     app.run(debug=False)
     
+    
+    
 
-#subprocess.call("refresh") 
+subprocess.call("refresh") 
     
           
     
