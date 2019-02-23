7d3af6b6066b10b0a8ac1e196c1848aff59f9770
Glitch (gdpd-draft) <none>
2019-02-23 17:31:32 +0000

    🏇👴 Checkpoint ./templates/calendar.html:706703/551 ./server.py:706703/11 ./templates/index.html:706703/313

--- server.py
+++ server.py
@@ -17,6 +17,7 @@ app = Flask(__name__)
 
 
 
+
 @app.route('/')
 def index():
      return render_template('index.html')
@@ -27,6 +28,8 @@ def calendar():
   
 if __name__ == '__main__':
     app.run(debug=False)
+    
+    
     
     
     
