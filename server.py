8d70b0e983a98822bd87906e8ece678a9f607657
Glitch (gdpd-draft) <none>
2019-03-17 12:16:10 +0000

    🍞😻 Checkpoint ./server.py:706703/405 ./requirements.txt:706703/20 ./start.sh:706703/1

--- server.py
+++ server.py
@@ -197,4 +197,18 @@ def cposter():
 if __name__ == '__main__':
     app.run(debug=False)
     
+from git import Repo
+
+repo_dir = 'mathematics'
+repo = Repo(repo_dir)
+file_list = [
+    'numerical_analysis/regression_analysis/simple_regression_analysis.py',
+    'numerical_analysis/regression_analysis/simple_regression_analysis.png'
+]
+commit_message = 'Add simple regression analysis'
+repo.index.add(file_list)
+repo.index.commit(commit_message)
+origin = repo.remote('origin')
+origin.push()
+    
 #subprocess.call("refresh")
