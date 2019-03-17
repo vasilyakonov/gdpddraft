6fba594bf1887a7c8390dbd58712fbf592dba723
Glitch (gdpd-draft) <none>
2019-03-17 12:38:08 +0000

    ⚡️👩‍❤️‍💋‍👩 Checkpoint ./github.py:706703/112 ./requirements.txt:706703/6

--- github.py
+++ github.py
@@ -1,7 +1,7 @@
-import gitpython
+# -*- coding: utf-8 -*-
 from git import Repo
 
-repo_dir = 'GDPD-Draft'
+repo_dir = 'templates'
 repo = Repo(repo_dir)
 file_list = [
     'templates/hypergraphx.html',
@@ -10,5 +10,5 @@ file_list = [
 commit_message = 'Add simple regression analysis'
 repo.index.add(file_list)
 repo.index.commit(commit_message)
-origin = repo.remote('origin')
+origin = repo.remote('https://github.com/vasilyakonov/GDPD-Draft.git')
 origin.push()
