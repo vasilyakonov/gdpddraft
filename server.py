ab63a9221e31f509f264405c298f7a85c3badb12
Glitch (gdpd-draft) <none>
2019-04-18 22:56:54 +0000

    '42b7dc90-622d-11e9-a3ba-0242ac11008e'

--- server.py
+++ server.py
@@ -16,7 +16,6 @@ u = uuid.uuid1()
 from bs4 import BeautifulSoup 
 
 
-
 """markup = <div class="object_one"> <h1>Journal For Raw Visual Data</h1> 
       Praesent efficitur, nibh vitae fringilla scelerisque, est neque faucibus quam, in iaculis purus libero eget mauris. Curabitur et luctus sapien, ac gravida orci. Aliquam erat volutpat. In hac habitasse platea dictumst. Aenean commodo, arcu a commodo efficitur, libero dolor mollis turpis, non posuere orci leo eget enim. Curabitur sit amet elementum orci, pulvinar dignissim urna. Morbi id ex eu ex congue laoreet. Aenean tincidunt dolor justo, semper pretium libero luctus nec. Ut vulputate metus accumsan leo imperdiet tincidunt. Phasellus nec rutrum dolor. Cras imperdiet sollicitudin arcu, id interdum nibh fermentum in.
     </div>
